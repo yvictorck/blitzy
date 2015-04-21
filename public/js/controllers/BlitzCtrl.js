78424123dca54fab279986e2cb05310d71d6f9c4
chenyu-an <victor.chen@dowjones.com>
2015-04-21 16:37:37 -0400

    fix cols

--- public/js/controllers/BlitzCtrl.js
+++ public/js/controllers/BlitzCtrl.js
@@ -206,7 +206,8 @@ angular.module('BlitzCtrl', []).controller('BlitzController', function($scope,$h
 					  // $scope.mina_total = mina_total;
 					  // $scope.jess_total = jess_total;
 					  // $scope.vic_total = vic_total;
-					  $scope.tagline = current;
+					  
+					  // $scope.tagline = current;
 						$scope.kyung_one_round = "";
 						$scope.mina_one_round = "";
 						$scope.jess_one_round ="";
